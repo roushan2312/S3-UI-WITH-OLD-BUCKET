df2ac6bf35122a3cb1e25749815d3762154e4b92
roushan2312 <barnwal23roushan@gmail.com>
2026-02-22 23:06:42 +0530

    vb

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -11,42 +11,34 @@ const customBucketName2 = "1vp-test-textract";
 
 backend.addOutput({
   version: "1.3",
-  storage: [    
-    {
+  storage: {
     aws_region: "ap-south-1",
-    bucket_name: customBucketName,
+    bucket_name: customBucketName, // primary bucket
     buckets: [
-        {
-          name: customBucketName,
-          bucket_name: customBucketName,
-          aws_region: "ap-south-1",
-          paths: {
-            "invoices/*": {
-              groupsadmin: ["get", "list", "write", "delete"],
-                groupsops: ["get", "list", "write"],
-            },
+      {
+        name: customBucketName,
+        bucket_name: customBucketName,
+        aws_region: "ap-south-1",
+        paths: {
+          "invoices/*": {
+            groupsadmin: ["get", "list", "write", "delete"],
+            groupsops: ["get", "list", "write"],
           },
         },
-      ],
-    },
-    {
-    aws_region: "ap-south-1",
-    bucket_name: customBucketName2,
-    buckets: [
-        {
-          name: customBucketName2,
-          bucket_name: customBucketName2,
-          aws_region: "ap-south-1",
-          paths: {
-            "test-folder/*": {
-              groupsadmin: ["get", "list", "write", "delete"],
-                groupsops: ["get", "list", "write"],
-            },
+      },
+      {
+        name: customBucketName2,
+        bucket_name: customBucketName2,
+        aws_region: "ap-south-1",
+        paths: {
+          "test-folder/*": {
+            groupsadmin: ["get", "list", "write", "delete"],
+            groupsops: ["get", "list", "write"],
           },
         },
-      ],
-    },
-  ]
+      },
+    ],
+  },
 });
 
 
