c28d7b5306c7fb62bf8f920913ab4fba012e4be1
roushan2312 <barnwal23roushan@gmail.com>
2026-02-23 15:26:03 +0530

    v1.3

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,6 +7,6 @@ export const auth = referenceAuth({
   unauthRoleArn: 'arn:aws:iam::137068221002:role/service-role/guest-role', // Existing Unauth Role
   userPoolClientId: '3uq1a3hifu8m49n6b9p9mnk7pp', // Your existing App Client ID
   groups: {
-    admin: 'admin', // Maps the 'admin' reference to your existing 'admin' group name
+    admin: 'arn:aws:iam::137068221002:role/admin-role-s3-ui', // Maps the 'admin' reference to your existing 'admin' group name
   },
 });
